2be76d881e92f31c222ae79d396685cdc2c90d6e
infoocean <infoocean8454@gmail.com>
2022-11-18 18:59:45 +0530

    update18

--- src/API/APIToken.js
+++ src/API/APIToken.js
@@ -6,11 +6,11 @@ const auth_token =
 export default auth_token;
 
 //online server
-// const api = "https://mytaskbackendserver.herokuapp.com/";
-// export { api };
-//local server
-const api = "http://localhost:5000/";
+const api = "https://mytaskbackendserver.herokuapp.com/";
 export { api };
+//local server
+// const api = "http://localhost:5000/";
+// export { api };
 
 const woocemmerceapi = new WooCommerceRestApi({
   url: "https://furniture.mangoitsol.com",
